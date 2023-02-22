c6c0ee8f420d4f62fd2a452d5cc616b5ea758e4f
Nisha Chavan <nisha.chavan@lekab.com>
2023-02-22 15:07:07 +0100

    added delete and add in api

--- server/app.ts
+++ server/app.ts
@@ -11,17 +11,91 @@ const getAllCustomersInformation = (_req: Request, res: Response) => {
     res.status(200).json(customers);
 };
 
-const getCustomerById = (_req: Request, res: Response) => {
-    const id = (_req.params.id);
+const getCustomerById = (req: Request, res: Response) => {
+    const id = req.params.id;
     const customer = customers.find(c => c.id.value === id);
     if (customer) {
         res.status(200).json(customer);
     } else {
         res.status(404).json({
-            message: 'Puppy with id '+ id +' not found'
+            message: 'Customer with id '+ id +' not found'
         });
     }
 }
+
+const deleteCustomer = (req: Request, res: Response) => {
+    const id = req.params.id;
+    const customerToBeDeleted = customers.find(p => p.id.value === id);
+    if (customerToBeDeleted) {
+        customers.splice(customers.indexOf(customerToBeDeleted), 1);
+        res
+            .status(200)
+            .json({
+                message: 'Customer deleted successfully'
+            });
+    } else {
+        res.status(404).json({
+            message: 'Customer with id '+ id +' not found'
+        });
+    }
+};
+
+const addCustomer = (req: Request, res: Response) => {
+    const customerToBeAdded = {
+        gender: req.body.gender,
+        name: {
+            title: req.body.title,
+            first: req.body.first,
+            last: req.body.last
+        },
+        location: {
+            street: {
+                number: req.body.number,
+                name: req.body.name
+            },
+            city: req.body.city,
+            state: req.body.state,
+            country: req.body.country,
+            postcode: req.body.postcode,
+            coordinates: {
+                latitude: req.body.latitude,
+                longitude: req.body.longitude
+            },
+            timezone: {
+                offset: req.body.offset,
+                description: req.body.description
+            }
+        },
+        email: req.body.email,
+        dob: {
+            date: req.body.date,
+            age: req.body.age
+        },
+        registered: {
+            date: req.body.date,
+            age: req.body.age
+        },
+        phone: req.body.phone,
+        cell: req.body.cell,
+        id: {
+            name: req.body.name,
+            value: req.body.value
+        },
+        picture: {
+          large: req.body.large,
+          medium: req.body.medium,
+          thumbnail: req.body.thumbnail
+        },
+        nat: req.body.nat
+    }
+
+    customers.push(customerToBeAdded);
+    res
+        .status(201)
+        .json({
+            message: 'Customer added successfully'
+        });
+};
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({test: 'is working as it should'});
@@ -29,5 +103,8 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/customers', getAllCustomersInformation);
 app.get('/api/customers/:id', getCustomerById);
+app.delete('/api/customers/:id', deleteCustomer);
+app.post('/api/customers', addCustomer);
+
 
 export default app;
